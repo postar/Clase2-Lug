3bf7c805219500cd13c0c0318f0d045e37fda4dc
Mariano Tomás Pocztaruk <mpocztaruk_92@hotmail.com>
2022-08-25 08:40:15 -0300

    Add project files.

---
+++ Clase2-Lug/Acceso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Clase2_Lug
+{
+    internal class Acceso
+    {
+    }
+}
